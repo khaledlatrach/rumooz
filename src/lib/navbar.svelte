d2d2be81cbf167df49eb2430f3c53f02b9344468
khaled latrach <khaledlatrach4@gmail.com>
2024-01-30 22:04:38 +0100

    add the Navigation Bar

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,57 @@
+<div class="bg-neutral-700 text-neutral-400 text-lg">
+	<div class="flex h-20 w-11/12 max-w-7xl mx-auto items-center justify-between">
+		<!-- log -->
+		<div class="flex flex-col place-items-end">
+			<div class="flex gap-2">
+				<svg
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-8 h-8 bg-orange-600 rounded-full p-1 stroke-white"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+					/>
+				</svg>
+				<a href="/" class="">
+					RUMOOZ
+					<span class="text-neutral-500">.cc</span>
+				</a>
+			</div>
+		</div>
+		<!-- Middle menu -->
+		<div class=" gap-4 hidden md:flex">
+			<a href="/" class="py-1 hover:underline">الدروس</a>
+			<a href="/" class="py-1 hover:underline">المسارات</a>
+			<!-- <a href="/" class="hover:bg-slate-600 py-1 px-2 rounded-md hover:shadow-md">إتصل بنا</a>
+			<a href="/" class="hover:bg-slate-600 py-1 px-2 rounded-md hover:shadow-lg">من نحن</a> -->
+		</div>
+		<!-- Right menu -->
+		<a
+			href="/"
+			class="rounded-full hidden md:flex border border-neutral-400 px-2 pb-1 hover:border-neutral-100"
+			>تسجيل الدخول</a
+		>
+
+		<!-- Small screen -->
+		<div class="flex md:hidden">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				/>
+			</svg>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Navbar from '$lib/navbar.svelte';
+	//import Hero from '$lib/hero.svelte';
+</script>
+
+<Navbar />
